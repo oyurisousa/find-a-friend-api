c45a0349040ef5d08dfbb6ee4a1f8c180baf760e
yuri <yurisousaenfer@gmail.com>
2024-09-28 22:07:42 -0300

    initial commit

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const schemaEnv = z.object({
+  NODE_ENV: z.enum(['dev', 'production', 'test']).default('dev'),
+  PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
+  DATABASE_URL: z.string(),
+})
+
+const _env = schemaEnv.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠ Invalid environment', _env.error.format())
+  throw new Error('Invalid environments variables!')
+}
+
+export const env = _env.data
